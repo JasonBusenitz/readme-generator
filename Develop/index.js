d64eba9a0d1bd7dc31d638597bc4b23560c6eb5c
unknown <jbus731@gmail.com>
2024-08-12 20:58:08 -0500

    partially completed the reade me template

--- Develop/index.js
+++ Develop/index.js
@@ -9,52 +9,52 @@ const questions = [
             [
                 {
                     type: 'list',
-                    name: 'license',
-                    choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License (GPL) v3', 'Mozilla Public License 2.0', 'BSD 3-Clause License'],
+                    name: 'data-license',
+                    choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License (GPL) v3', 'Mozilla Public License 2.0', 'BSD 3-Clause License', 'None'],
                 },
                 {
                     type: 'input',
-                    name: 'title',
+                    name: 'data-title',
                     message: 'What is the title of your application?',
                 },
                 {
                     type: 'input',
-                    name: 'description',
+                    name: 'data-description',
                     message: 'Write a few sentances describing your application',
                 },
                 {
                     type: 'input',
-                    name: 'install',
+                    name: 'data-install',
                     message: 'How would a user install your application?',
                 },
                 {
                     type: 'input',
-                    name: 'usage',
+                    name: 'data-usage',
                     message: 'How does a user use your application?',
                 },
                 {
                     type: 'input',
-                    name: 'contribution',
+                    name: 'data-contribution',
                     message: 'How can other developers contribut to your application? If they can not simply put N/A',
                 },
                 {
                     type: 'input',
-                    name: 'tests',
+                    name: 'data-tests',
                     message: 'This is where you pate tests for your application, as well as instructions on how to use those tests.',
                 },
                 {
                     type: 'input',
-                    name: 'gitHUbUserName',
+                    name: 'data-gitHUbUserName',
                     message: 'What is your Git Hub username?',
                 },
                 {
                     type: 'input',
-                    name: 'gitHUbLink',
+                    name: 'data-gitHUbLink',
                     message: 'Paste a link to your Git Hub account here.',
                 },
                 {
                     type: 'input',
-                    name: 'email',
+                    name: 'data-email',
                     message: 'What is a good email for people to reach you at?',
                 }
             ]
